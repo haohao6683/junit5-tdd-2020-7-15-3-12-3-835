d9dc472f87fd2b01266d41055cebd3a46f41f477
DRAYMOND M H XIE (ITA-ISDC-ISD-OOCLL/ZHA) <XIEDR2@OOCL.COM>
2020-07-23 19:09:36 +0800

    [Draymond] feature:Adding an implement of should_return_5_when_play_fizzBuzz_given_5

--- src/test/java/junit5/tdd/FizzBuzzTest.java
+++ src/test/java/junit5/tdd/FizzBuzzTest.java
@@ -33,4 +33,16 @@ public class FizzBuzzTest {
         assertEquals("Fizz", actual);
     }
 
+    @Test
+    void should_return_5_when_play_fizzBuzz_given_5() {
+        //given
+        int number = 5;
+
+        //when
+        FizzBuzz fizz = new FizzBuzz();
+        String actual = fizz.play(number);
+
+        //then
+        assertEquals("Buzz", actual);
+    }
 }
